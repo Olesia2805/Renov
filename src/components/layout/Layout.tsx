0b62a1f5474ac5dcbd3f4a463cbc5f73d4a5b610
Olesia Vysotina <vysotina2002@gmail.com>
2026-03-02 22:43:04 +0100

    update layout components and add them to Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import TestimonialsSection from "./TestimonialsSection";
+import FAQSection from "./FAQSection";
+import Subscription from "./Subscription";
 
 const Layout = () => {
   return (
     <div>
+      <Header />
       <main>
         <Outlet />
+        <TestimonialsSection />
+        <FAQSection />
+        <Subscription />
       </main>
+      <Footer />
     </div>
   );
 };
