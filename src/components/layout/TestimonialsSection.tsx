4fd140a5e6f018fae4f71bba6c74c9a4559949af
Borys Boichenko <boris02374@gmail.com>
2026-05-09 16:01:41 +0000

    fixed layout

--- src/components/layout/TestimonialsSection.tsx
+++ src/components/layout/TestimonialsSection.tsx
@@ -32,31 +32,22 @@
 
 const Testimonials = () => {
   return (
-    /* Frame 1321316144 */
-
-    /* Auto layout */
-    // display: flex;
-    // flex-direction: column;
-    // align-items: flex-start;
-    // padding: 32px;
-    // gap: 179px;
-    // isolation: isolate;
-
-    // position: absolute;
-    // width: 845px;
-    // height: 559px;
-    // left: 80px;
-    // top: 60px;
-
-    // background: #F5AA21;yellow-2
-    // border-radius: 12px;
-
     <section className="bg-white w-full max-w-360 min-h-169.75 mx-auto py-15 px-20 flex flex-col justify-center">
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-end gap-6 mb-10 md:mb-16">
-        <div className="bg-[#F5AA21] rounded-xl p-8 flex flex-col items-start isolate w-full lg:max-w-211.25 min-h-139.75">
-          <h2>Hear From Our Satisfied Customers</h2>
-        </div>
-      </div>
+      <ul className="flex flex-col md:flex-row justify-between items-start md:items-end gap-6 mb-10 md:mb-16">
+        <li className="bg-[#F5AA21] rounded-xl p-8 flex flex-col items-start isolate w-full lg:max-w-211.25 min-h-139.75">
+          <h2 className="w-full lg:w-101.75 min-h-28 font-['Inter_Tight'] text-[48px] leading-14 text-[#070707] font-medium ">
+            Hear From Our Satisfied Customers
+          </h2>
+          <div>
+            <ul></ul>
+            <p></p>
+          </div>
+        </li>
+        <li className="bg-[#f5f5f5] rounded-xl p-8 flex flex-col items-start  w-full lg:max-w-102.75 min-h-139.75">
+          <div className="  "></div>
+          <div className=" "></div>
+        </li>
+      </ul>
     </section>
   );
 };
